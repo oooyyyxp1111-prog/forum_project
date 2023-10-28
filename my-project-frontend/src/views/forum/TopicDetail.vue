b96d06b1daa2130c617e0dcc04419f2455afb6b1
NagoColer <nagocoler@qq.com>
2023-10-28 18:13:51 +0800

    完成评论之间的回复，完成评论删除，修复之前的小问题

--- my-project-frontend/src/views/forum/TopicDetail.vue
+++ my-project-frontend/src/views/forum/TopicDetail.vue
@@ -3,7 +3,7 @@ import {useRoute} from "vue-router";
 import {get, post} from "@/net";
 import axios from "axios";
 import {computed, reactive, ref} from "vue";
-import {ArrowLeft, CircleCheck, EditPen, Female, Male, Plus, Star} from "@element-plus/icons-vue";
+import {ArrowLeft, ChatSquare, CircleCheck, Delete, EditPen, Female, Male, Plus, Star} from "@element-plus/icons-vue";
 import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html';
 import Card from "@/components/Card.vue";
 import router from "@/router";
@@ -30,7 +30,7 @@ const edit = ref(false)
 const comment = reactive({
     show: false,
     text: '',
-    quote: -1
+    quote: null
 })
 
 const init = () => get(`api/forum/topic?tid=${tid}`, data => {
@@ -79,6 +79,13 @@ function loadComments(page) {
 function onCommentAdd() {
     comment.show = false
     loadComments(Math.floor(++topic.data.comments / 10) + 1)
+}
+
+function deleteComment(id) {
+    get(`/api/forum/delete-comment?id=${id}`, () => {
+        ElMessage.success('删除评论成功！')
+        loadComments(topic.page)
+    })
 }
 </script>
 
@@ -170,7 +177,16 @@ function onCommentAdd() {
                         <div style="font-size: 13px;color: grey">
                             <div>评论时间: {{new Date(item.time).toLocaleString()}}</div>
                         </div>
+                        <div v-if="item.quote" class="comment-quote">
+                            回复: {{item.quote}}
+                        </div>
                         <div class="topic-content" v-html="convertToHtml(item.content)"></div>
+                        <div style="text-align: right">
+                            <el-link :icon="ChatSquare" @click="comment.show = true;comment.quote = item"
+                                     type="info">&nbsp;回复评论</el-link>
+                            <el-link :icon="Delete" type="danger" v-if="item.user.id === store.user.id"
+                                     style="margin-left: 20px" @click="deleteComment(item.id)">&nbsp;删除评论</el-link>
+                        </div>
                     </div>
                 </div>
                 <div style="width: fit-content;margin: 20px auto">
@@ -186,13 +202,22 @@ function onCommentAdd() {
                       :default-title="topic.data.title" submit-button="更新帖子内容" :submit="updateTopic"/>
         <topic-comment-editor :show="comment.show" @close="comment.show = false" :tid="tid"
                               :quote="comment.quote" @comment="onCommentAdd"/>
-        <div class="add-comment" @click="comment.show = true">
+        <div class="add-comment" @click="comment.show = true;comment.quote = null">
             <el-icon><Plus/></el-icon>
         </div>
     </div>
 </template>
 
 <style lang="less" scoped>
+.comment-quote {
+    font-size: 13px;
+    color: grey;
+    background-color: rgba(94, 94, 94, 0.1);
+    padding: 10px;
+    margin-top: 10px;
+    border-radius: 5px;
+}
+
 .add-comment {
     position: fixed;
     bottom: 20px;
