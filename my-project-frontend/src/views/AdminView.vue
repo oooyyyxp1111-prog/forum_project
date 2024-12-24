dc0dfbc03762baa8a7e56416de1bf7b13c7ed0a8
nagocoler <nagocoler@qq.com>
2024-12-25 01:53:35 +0800

    添加管理端Tab栏

--- my-project-frontend/src/views/AdminView.vue
+++ my-project-frontend/src/views/AdminView.vue
@@ -12,13 +12,15 @@ import {
     User
 } from "@element-plus/icons-vue";
 import UserInfo from "@/components/UserInfo.vue";
-import {inject} from "vue";
+import {inject, onMounted, ref} from "vue";
+import router from "@/router";
+import {useRoute} from "vue-router";
 
 const adminMenu = [
     {
         title: '校园论坛管理', icon: Location, sub: [
-            { title: '用户管理', icon: User },
-            { title: '帖子广场管理', icon: ChatDotSquare },
+            { title: '用户管理', icon: User, index: '/admin/user' },
+            { title: '帖子广场管理', icon: ChatDotSquare, index: '/admin/forum' },
             { title: '失物招领管理', icon: Bell },
             { title: '校园活动管理', icon: Notification },
             { title: '表白墙管理', icon: Umbrella },
@@ -35,7 +37,46 @@ const adminMenu = [
     }
 ]
 
+const route = useRoute()
 const loading = inject('userLoading')
+const pageTabs = ref([])
+
+function handleTabClick({ props }) {
+    router.push(props.name)
+}
+
+function handleTabClose(name) {
+    const index = pageTabs.value.findIndex(tab => tab.name === name)
+    const isCurrent = name === route.fullPath
+    pageTabs.value.splice(index, 1)
+    if(pageTabs.value.length > 0) {
+        //删除后，标签列表中还有剩余的Tab且关闭的是当前的，则自动进行切换，默认切换到上一个，如果没有上一个，则切换到下一个
+        if(isCurrent) {
+            router.push(pageTabs.value[Math.max(0, index - 1)].name)
+        }
+    } else {
+        router.push('/admin')
+    }
+}
+
+function addAdminTab(menu) {
+    if(!menu.index) return
+    if(pageTabs.value.findIndex(tab => tab.name === menu.index) < 0) {
+        pageTabs.value.push({
+            title: menu.title,
+            name: menu.index
+        })
+    }
+}
+
+onMounted(() => {
+    const initPage = adminMenu
+        .flatMap(menu => menu.sub)
+        .find(sub => sub.index === route.fullPath)
+    if(initPage) {
+        addAdminTab(initPage)
+    }
+})
 </script>
 
 <template>
@@ -59,7 +100,9 @@ const loading = inject('userLoading')
                                 </el-icon>
                                 <span><b>{{ menu.title }}</b></span>
                             </template>
-                            <el-menu-item :index="subMenu.index" v-for="subMenu in menu.sub">
+                            <el-menu-item :index="subMenu.index"
+                                          @click="addAdminTab(subMenu)"
+                                          v-for="subMenu in menu.sub">
                                 <template #title>
                                     <el-icon>
                                         <component :is="subMenu.icon"/>
@@ -73,10 +116,27 @@ const loading = inject('userLoading')
             </el-aside>
             <el-container>
                 <el-header class="admin-content-header">
-                    <div style="flex: 1"></div>
+                    <div style="flex: 1">
+                        <el-tabs type="card"
+                                 :model-value="route.fullPath"
+                                 closable
+                                 @tab-remove="handleTabClose"
+                                 @tab-click="handleTabClick">
+                            <el-tab-pane v-for="tab in pageTabs"
+                                         :label="tab.title"
+                                         :name="tab.name"
+                                         :key="tab.name"/>
+                        </el-tabs>
+                    </div>
                     <user-info/>
                 </el-header>
-                <el-main>Main</el-main>
+                <el-main>
+                    <router-view v-slot="{ Component }">
+                        <keep-alive>
+                            <component :is="Component" />
+                        </keep-alive>
+                    </router-view>
+                </el-main>
             </el-container>
         </el-container>
     </div>
@@ -107,6 +167,24 @@ const loading = inject('userLoading')
         display: flex;
         align-items: center;
         box-sizing: border-box;
+
+        :deep(.el-tabs__header) {
+            height: 32px;
+            margin-bottom: 0;
+            border-bottom: none;
+        }
+
+        :deep(.el-tabs__nav) {
+            gap: 10px;
+            border: none;
+        }
+
+        :deep(.el-tabs__item) {
+            height: 32px;
+            padding: 0 15px;
+            border-radius: 6px;
+            border: solid 1px var(--el-border-color);
+        }
     }
 }
 </style>
---
+++ my-project-frontend/src/views/admin/UserAdmin.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        我是用户管理
+    </div>
+</template>
+
+<style scoped>
+
+</style>
